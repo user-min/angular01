4c88502f08a1fc7078293b23e574fa24a0f3bdb0
zhaomin <2014467593@qq.com>
2020-09-23 09:22:16 +0800

    feat(angular)：主要看app-routing.module.ts与one-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,11 @@ import {Routes, RouterModule} from '@angular/router';
 
 const routes: Routes = [
   {path: '', redirectTo: 'one', pathMatch: 'full'},
+  {path: 'one', loadChildren: 'src/app/routes/one/one.module'}
+
+  // 方法可用，但是路由不正确
+  // {path: 'first', component: FirstComponent},
+  // {path: 'second', component: SecondComponent}
 ];
 
 @NgModule({
