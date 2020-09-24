175423dff76a7ea580486627305048a182316c9e
zhaomin <2014467593@qq.com>
2020-09-24 09:03:45 +0800

    fix(angular)：创建类，特殊在【构造函数，以及它的使用上】，具体内容参考 src\app\core\model\class-test.ts与 src\app\routes\one\second\second.component.ts

---
+++ src/app/core/model/class-test.ts
@@ -0,0 +1,7 @@
+/* 定义了一个类，具有一个构造函数和两个属性：id与name*/
+export class ClassTest {
+  constructor(
+    public id: string,
+    public name: string) {
+  }
+}
--- src/app/routes/one/second/second.component.ts
+++ src/app/routes/one/second/second.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ClassTest} from '../../../core/model/class-test';
+
 
 @Component({
   selector: 'app-second',
   templateUrl: './second.component.html',
   styleUrls: ['./second.component.less']
 })
+
 export class SecondComponent implements OnInit {
 
-  constructor() { }
+  heroes1 = ['11', '22', '33'];
+
+  heroes2 = [
+    new ClassTest('1', 'test1'),
+    new ClassTest('2', 'test2'),
+    new ClassTest('3', 'test3'),
+    new ClassTest('4', 'test4')
+  ];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
