13335450bf4ee853d7209e68b9e4d2fbd546721d
赵敏 <2014467593@qq.com>
2020-09-27 11:49:34 +0800

    fix(angular01): 1.添加基本知识； 2.解决TS2300: Duplicate identifier问题.   方式：   【在tsconfig.json的compilerOptions中加入两项："skipLibCheck": true,"allowSyntheticDefaultImports": true   】

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+/*模块*/
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
-import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { registerLocaleData } from '@angular/common';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NgZorroAntdModule, NZ_I18N, zh_CN} from 'ng-zorro-antd';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {registerLocaleData} from '@angular/common';
 import zh from '@angular/common/locales/zh';
 import {OneModule} from './routes/one/one.module';
 
 registerLocaleData(zh);
 
 @NgModule({
+  /*声明模块中有什么东西， 只能声明组件、指令、管道*/
   declarations: [
     AppComponent
   ],
+  /*声明该模块所依赖的模块*/
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -27,7 +30,12 @@ registerLocaleData(zh);
 
     OneModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+  /*声明模块中听过的服务，默认为空*/
+  providers: [
+    {provide: NZ_I18N, useValue: zh_CN}
+  ],
+  /*声明模块的主组件*/
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
